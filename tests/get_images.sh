6705f6e08fc4aead7b910f18f0c5c37663530b05
emily <emilymuller1991@gmail.com>
2022-06-17 10:12:19 +0100

    download_images

--- tests/get_images.sh
+++ tests/get_images.sh
@@ -1,3 +1,4 @@
-mkdir $ROOT/download_images/outputs/downloaded/
+mkdir -p $ROOT/download_images/outputs/
+mkdir -p $ROOT/download_images/outputs/downloaded/
 
 cat $ROOT/tests/test_input/test_city00 | parallel --delay 1.5 --joblog /tmp/log --pipe --block 2M --ungroup python3 $ROOT/download_images/functions/get_images.py
